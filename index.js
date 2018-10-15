aad02089d1b42e63c798039b38f6d4c658e5b111
Austin Wright <aaa@bzfx.net>
2018-10-14 17:38:25 -0700

    Fix PassThrough and such

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
 module.exports.ServerResponseTransform = require('./lib/ServerResponseTransform.js').ServerResponseTransform;
+module.exports.ServerResponsePassThrough = require('./lib/PassThrough.js').PassThrough;
 module.exports.PassThrough = require('./lib/PassThrough.js').PassThrough;
 
 
