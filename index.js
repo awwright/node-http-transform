4c3ffe58b015a4caeab033930c7ed713616481d3
Austin Wright <aaa@bzfx.net>
2019-04-26 13:07:30 -0700

    use strict

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 module.exports.ServerResponseTransform = require('./lib/ServerResponseTransform.js').ServerResponseTransform;
 module.exports.ServerResponsePassThrough = require('./lib/PassThrough.js').PassThrough;
