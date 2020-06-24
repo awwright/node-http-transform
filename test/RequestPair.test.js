5892f52b2771bca5ae348d1cde25b2893b6e3fe8
Austin Wright <aaa@bzfx.net>
2020-06-23 17:17:21 -0700

    Add serverWritableSide/clientReadableSide to ResponsePassThrough

--- test/RequestPair.test.js
+++ test/RequestPair.test.js
@@ -5,158 +5,162 @@ var assert = require('assert');
 var lib = require('..');
 
 describe('makeRequestPair', function(){
-	describe('clientWritableSide instanceof ClientRequest', function(){
-		it('abort()');
-		it('setTimeout()');
-		it('clearTimeout()');
-		it('setNoDelay()');
-	});
-	describe('clientWritableSide instanceof OutgoingMessage', function(){
-		var pair, clientWritableSide, serverReadableSide;
-		beforeEach(function(){
-			pair = lib.makeRequestPair();
-			clientWritableSide = pair.clientWritableSide;
-			serverReadableSide = pair.serverReadableSide;
+	describe('clientWritableSide', function(){
+		describe('instanceof ClientRequest', function(){
+			it('abort()');
+			it('setTimeout()');
+			it('clearTimeout()');
+			it('setNoDelay()');
 		});
-		// it('clientWritableSide instanceof OutgoingMessage', function(){
-		// 	assert(clientWritableSide instanceof http.OutgoingMessage);
-		// });
-		it('setHeader(name, value)', function(){
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			clientWritableSide.end();
-			return serverReadableSide.ready.then(function(){
-				assert.strictEqual(serverReadableSide.headers['accept'], 'GET, HEAD, POST');
+		describe('instanceof OutgoingMessage', function(){
+			var pair, clientWritableSide, serverReadableSide;
+			beforeEach(function(){
+				pair = lib.makeRequestPair();
+				clientWritableSide = pair.clientWritableSide;
+				serverReadableSide = pair.serverReadableSide;
 			});
-		});
-		it('getHeader(name)', function(){
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			assert.strictEqual(clientWritableSide.getHeader('ACCEPT'), 'GET, HEAD, POST');
-		});
-		it('getHeaderNames()', function(){
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			clientWritableSide.setHeader('Content-Type', 'application/xhtml+xml');
-			assert.deepStrictEqual(clientWritableSide.getHeaderNames(), ['accept', 'content-type']);
-		});
-		it('getHeaders()', function(){
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			clientWritableSide.setHeader('Content-Type', 'application/xhtml+xml');
-			const headers = clientWritableSide.getHeaders();
-			// FIXME is this expecting [Object: null prototype] ?
-			assert.deepStrictEqual(headers['accept'], 'GET, HEAD, POST');
-			assert.deepStrictEqual(headers['content-type'], 'application/xhtml+xml');
-		});
-		it('hasHeader()', function(){
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			assert.strictEqual(clientWritableSide.hasHeader('ACCEPT'), true);
-			assert.strictEqual(clientWritableSide.hasHeader('Foo'), false);
-		});
-		it('removeHeader()', function(){
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			clientWritableSide.removeHeader('Accept');
-			assert.strictEqual(clientWritableSide.getHeader('Accept'), undefined);
-			assert.strictEqual(clientWritableSide.getHeader('ACCEPT'), undefined);
-			assert.strictEqual(clientWritableSide.hasHeader('Accept'), false);
-		});
-		it('flushHeaders()', function(){
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			clientWritableSide.flushHeaders();
-			return serverReadableSide.ready.then(function(){
-				assert.strictEqual(serverReadableSide.headers['accept'], 'GET, HEAD, POST');
+			// it('clientWritableSide instanceof OutgoingMessage', function(){
+			// 	assert(clientWritableSide instanceof http.OutgoingMessage);
+			// });
+			it('setHeader(name, value)', function(){
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				clientWritableSide.end();
+				return serverReadableSide.ready.then(function(){
+					assert.strictEqual(serverReadableSide.headers['accept'], 'GET, HEAD, POST');
+				});
 			});
-		});
-		it('write()', function(done){
-			clientWritableSide.write('Content\r\n');
-			serverReadableSide.setEncoding('UTF-8');
-			serverReadableSide.on('readable', function(){
-				assert.strictEqual(this.read(), 'Content\r\n');
-				done();
+			it('getHeader(name)', function(){
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				assert.strictEqual(clientWritableSide.getHeader('ACCEPT'), 'GET, HEAD, POST');
 			});
-		});
-		it('end()', function(done){
-			clientWritableSide.end('Content\r\n');
-			serverReadableSide.setEncoding('UTF-8');
-			var data = '';
-			serverReadableSide.on('readable', function(){
-				for(var s; null !== (s=this.read());) data += s;
+			it('getHeaderNames()', function(){
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				clientWritableSide.setHeader('Content-Type', 'application/xhtml+xml');
+				assert.deepStrictEqual(clientWritableSide.getHeaderNames(), ['accept', 'content-type']);
 			});
-			serverReadableSide.on('end', function(){
-				assert.strictEqual(data, 'Content\r\n');
-				done();
+			it('getHeaders()', function(){
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				clientWritableSide.setHeader('Content-Type', 'application/xhtml+xml');
+				const headers = clientWritableSide.getHeaders();
+				// FIXME is this expecting [Object: null prototype] ?
+				assert.deepStrictEqual(headers['accept'], 'GET, HEAD, POST');
+				assert.deepStrictEqual(headers['content-type'], 'application/xhtml+xml');
 			});
-		});
-		it('addTrailers()');
-		it('pipe()', function(done){
-			clientWritableSide.method = 'POST';
-			clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
-			clientWritableSide.end('Content\r\n');
+			it('hasHeader()', function(){
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				assert.strictEqual(clientWritableSide.hasHeader('ACCEPT'), true);
+				assert.strictEqual(clientWritableSide.hasHeader('Foo'), false);
+			});
+			it('removeHeader()', function(){
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				clientWritableSide.removeHeader('Accept');
+				assert.strictEqual(clientWritableSide.getHeader('Accept'), undefined);
+				assert.strictEqual(clientWritableSide.getHeader('ACCEPT'), undefined);
+				assert.strictEqual(clientWritableSide.hasHeader('Accept'), false);
+			});
+			it('flushHeaders()', function(){
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				clientWritableSide.flushHeaders();
+				return serverReadableSide.ready.then(function(){
+					assert.strictEqual(serverReadableSide.headers['accept'], 'GET, HEAD, POST');
+				});
+			});
+			it('write()', function(done){
+				clientWritableSide.write('Content\r\n');
+				serverReadableSide.setEncoding('UTF-8');
+				serverReadableSide.on('readable', function(){
+					assert.strictEqual(this.read(), 'Content\r\n');
+					done();
+				});
+			});
+			it('end()', function(done){
+				clientWritableSide.end('Content\r\n');
+				serverReadableSide.setEncoding('UTF-8');
+				var data = '';
+				serverReadableSide.on('readable', function(){
+					for(var s; null !== (s=this.read());) data += s;
+				});
+				serverReadableSide.on('end', function(){
+					assert.strictEqual(data, 'Content\r\n');
+					done();
+				});
+			});
+			it('addTrailers()');
+			it('pipe()', function(done){
+				clientWritableSide.method = 'POST';
+				clientWritableSide.setHeader('Accept', 'GET, HEAD, POST');
+				clientWritableSide.end('Content\r\n');
 
-			const pipe = lib.makeRequestPair();
-			serverReadableSide.pipe(pipe.clientWritableSide);
+				const pipe = lib.makeRequestPair();
+				serverReadableSide.pipe(pipe.clientWritableSide);
 
-			var data = '';
-			pipe.serverReadableSide.setEncoding('UTF-8');
-			pipe.serverReadableSide.on('headers', function(){
-				assert.strictEqual(pipe.serverReadableSide, this);
-				assert.strictEqual(this.method, 'POST');
-				assert.strictEqual(this.headers['accept'], 'GET, HEAD, POST');
-			});
-			pipe.serverReadableSide.on('readable', function(){
-				assert.strictEqual(pipe.serverReadableSide, this);
-				for(var s; null !== (s=this.read());) data += s;
-			});
-			pipe.serverReadableSide.on('end', function(){
-				assert.strictEqual(pipe.serverReadableSide, this);
-				assert.strictEqual(this.method, 'POST');
-				assert.strictEqual(this.headers['accept'], 'GET, HEAD, POST');
-				assert.strictEqual(data, 'Content\r\n');
-				done();
+				var data = '';
+				pipe.serverReadableSide.setEncoding('UTF-8');
+				pipe.serverReadableSide.on('headers', function(){
+					assert.strictEqual(pipe.serverReadableSide, this);
+					assert.strictEqual(this.method, 'POST');
+					assert.strictEqual(this.headers['accept'], 'GET, HEAD, POST');
+				});
+				pipe.serverReadableSide.on('readable', function(){
+					assert.strictEqual(pipe.serverReadableSide, this);
+					for(var s; null !== (s=this.read());) data += s;
+				});
+				pipe.serverReadableSide.on('end', function(){
+					assert.strictEqual(pipe.serverReadableSide, this);
+					assert.strictEqual(this.method, 'POST');
+					assert.strictEqual(this.headers['accept'], 'GET, HEAD, POST');
+					assert.strictEqual(data, 'Content\r\n');
+					done();
+				});
 			});
 		});
 	});
-	describe('serverReadableSide instanceof IncomingMessage', function(){
-		var pair, clientWritableSide, serverReadableSide;
-		beforeEach(function(){
-			pair = lib.makeRequestPair({}, {
-				path: '/foo',
-				method: 'POST',
-				headers: {Accept: 'text/plain, application/json'},
+	describe('serverReadableSide', function(){
+		describe('instanceof IncomingMessage', function(){
+			var pair, clientWritableSide, serverReadableSide;
+			beforeEach(function(){
+				pair = lib.makeRequestPair({}, {
+					path: '/foo',
+					method: 'POST',
+					headers: {Accept: 'text/plain, application/json'},
+				});
+				serverReadableSide = pair.serverReadableSide;
+				clientWritableSide = pair.clientWritableSide;
+				// clientWritableSide.writeHead(400, 'Message', {Accept: 'text/plain, application/json'});
+				// clientWritableSide.flushHeaders();
+				clientWritableSide.end('Content\r\n');
 			});
-			serverReadableSide = pair.serverReadableSide;
-			clientWritableSide = pair.clientWritableSide;
-			// clientWritableSide.writeHead(400, 'Message', {Accept: 'text/plain, application/json'});
-			// clientWritableSide.flushHeaders();
-			clientWritableSide.end('Content\r\n');
-		});
-		// it('serverReadableSide instanceof IncomingMessage', function(){
-		// 	assert(serverReadableSide instanceof http.IncomingMessage);
-		// });
-		it('rawTrailers');
-		it('trailers');
-		it('statusCode', function(){
-			return serverReadableSide.ready.then(function(){
-				assert.strictEqual(serverReadableSide.statusCode, null);
+			// it('serverReadableSide instanceof IncomingMessage', function(){
+			// 	assert(serverReadableSide instanceof http.IncomingMessage);
+			// });
+			it('rawTrailers');
+			it('trailers');
+			it('statusCode', function(){
+				return serverReadableSide.ready.then(function(){
+					assert.strictEqual(serverReadableSide.statusCode, null);
+				});
 			});
-		});
-		it('statusMessage', function(){
-			return serverReadableSide.ready.then(function(){
-				assert.strictEqual(serverReadableSide.statusMessage, null);
+			it('statusMessage', function(){
+				return serverReadableSide.ready.then(function(){
+					assert.strictEqual(serverReadableSide.statusMessage, null);
+				});
 			});
-		});
-		it('url', function(){
-			return serverReadableSide.ready.then(function(){
-				assert.strictEqual(serverReadableSide.url, '/foo');
+			it('url', function(){
+				return serverReadableSide.ready.then(function(){
+					assert.strictEqual(serverReadableSide.url, '/foo');
+				});
 			});
-		});
-		it('httpVersion');
-		it('headers', function(){
-			return serverReadableSide.ready.then(function(){
-				// FIXME maybe test it is [Object: null prototype]
-				assert.strictEqual(serverReadableSide.headers['accept'], 'text/plain, application/json');
+			it('httpVersion');
+			it('headers', function(){
+				return serverReadableSide.ready.then(function(){
+					// FIXME maybe test it is [Object: null prototype]
+					assert.strictEqual(serverReadableSide.headers['accept'], 'text/plain, application/json');
+				});
 			});
+			it('complete');
+			it('aborted');
+			it('setTimeout(msecs, callback)');
+			it('destroy(error)');
 		});
-		it('complete');
-		it('aborted');
-		it('setTimeout(msecs, callback)');
-		it('destroy(error)');
 	});
 });
--- test/ResponsePassThrough.test.js
+++ test/ResponsePassThrough.test.js
@@ -1,157 +1,206 @@
+"use strict";
 
 const assert = require('assert');
-const ResponsePassThrough = require('..').ResponsePassThrough;
-const PassThrough = require('stream').PassThrough;
 
-describe('ResponsePassThrough', function(){
-	describe('pipeMessage', function(){
-		var iin, iout;
+const lib = require('..');
+
+describe('makeResponsePair', function(){
+	describe('streams', function(){
+		var pair, serverWritableSide, clientReadableSide;
 		beforeEach(function(){
-			iin = new ResponsePassThrough;
-			iout = new ResponsePassThrough;
-			iin.pipe(iout);
+			pair = new lib.ResponsePassThrough();
+			serverWritableSide = pair.serverWritableSide;
+			clientReadableSide = pair.clientReadableSide;
+			console.log(pair);
 		});
+		it.skip('addTrailers on server are read by client', function(){
+			serverWritableSide.addTrailers({'Foo': 'Bar'});
+			serverWritableSide.end();
+			assert.strictEqual(clientReadableSide.trailers, 'x');
+		});
+		it('close on server is read by client', function(done){
+			serverWritableSide.write('x');
+			serverWritableSide.end();
+			clientReadableSide.resume();
+			clientReadableSide.on('end', done);
+		});
+	});
+	describe('serverWritableSide implements ServerResponse API', function(){
+		var pair, serverWritableSide, clientReadableSide;
+		beforeEach(function(){
+			pair = new lib.ResponsePassThrough();
+			serverWritableSide = pair.serverWritableSide;
+			clientReadableSide = pair.clientReadableSide;
+		});
+		// it('serverWritableSide instanceof ServerResponse', function(){
+		// 	assert(serverWritableSide instanceof http.ServerResponse);
+		// });
 		it('statusCode', function(){
-			iin.statusCode = 500;
-			assert.equal(iin.statusCode, 500);
-			iin.end();
-			return iout.ready.then(function(){
-				assert.equal(iout.statusCode, 500);
-			});
-		});
-		it('addHeader', function(){
-			iin.addHeader('Link', '<http://example.com/a>');
-			iin.addHeader('link', '<http://example.com/b>');
-			iin.addHeader('link', '<http://example.com/c>');
-			assert.equal(iin.getHeader('Link').length, 3);
-			iin.end();
-			return iout.ready.then(function(){
-				assert.equal(iout.getHeader('Link').length, 3);
+			serverWritableSide.statusCode = 400;
+			serverWritableSide.end();
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.statusCode, 400);
 			});
 		});
 		it('statusMessage', function(){
-			iin.statusMessage = 'Server Error';
-			assert.equal(iin.statusMessage, 'Server Error');
-			iin.end();
-			return iout.ready.then(function(){
-				assert.equal(iout.statusMessage, 'Server Error');
+			serverWritableSide.statusMessage = 'Custom Message';
+			serverWritableSide.end();
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.statusMessage, 'Custom Message');
 			});
 		});
-		it('hasHeader', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			assert(iin.hasHeader('Content-Type'));
-			iin.end();
-			return iout.ready.then(function(){
-				assert(iout.hasHeader('Content-Type'));
+		it('writeContinue()');
+		it('writeProcessing()');
+		it('writeHead(statusCode, statusMessage, headers)', function(){
+			serverWritableSide.writeHead(400, 'Message', {Allow: 'GET, HEAD, POST'});
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.statusCode, 400);
+				assert.strictEqual(clientReadableSide.statusMessage, 'Message');
+				assert.strictEqual(clientReadableSide.headers['allow'], 'GET, HEAD, POST');
 			});
-		});
-		it('getHeader', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			assert.equal(iin.getHeader('Content-Type'), 'text/plain');
-			iin.end();
-			return iout.ready.then(function(){
-				assert.equal(iout.getHeader('Content-Type'), 'text/plain');
-			});
-		});
-		it('removeHeader', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			assert(iin.hasHeader('Content-Type'));
-			iin.removeHeader('Content-Type');
-			assert(!iin.hasHeader('Content-Type'));
-			iin.end();
-			return iout.ready.then(function(){
-				assert(!iout.hasHeader('Content-Type'));
-			});
-		});
-		it('getHeaders', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			iin.end();
-			return iout.ready.then(function(){
-				assert.equal(Object.keys(iin.getHeaders()).length, 1);
-				assert.equal(Object.keys(iout.getHeaders()).length, 1);
-			});
-		});
-		it('getHeaderNames (empty)', function(){
-			iin.end();
-			return iout.ready.then(function(){
-				assert.equal(iin.getHeaderNames().length, 0);
-				assert.equal(iout.getHeaderNames().length, 0);
-			});
-		});
-		it('getHeaderNames', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			iin.end();
-			return iout.ready.then(function(){
-				assert.equal(iin.getHeaderNames().length, 1);
-				assert.equal(iout.getHeaderNames().length, 1);
-				// Node.js seems to normalize the names of the headers to lowercase for this function
-				assert.equal(iin.getHeaderNames()[0], 'content-type');
-				assert.equal(iout.getHeaderNames()[0], 'content-type');
-			});
-		});
-		it('headersReady', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			iin.end();
-			return iout.headersReady;
 		});
 	});
-	describe('pipeBody', function(){
-		var iin, iout;
+	describe('serverWritableSide implements OutgoingMessage API', function(){
+		var pair, serverWritableSide, clientReadableSide;
 		beforeEach(function(){
-			iin = new ResponsePassThrough;
-			iout = new ResponsePassThrough;
-			iin.pipeBody(iout);
+			pair = new lib.ResponsePassThrough();
+			serverWritableSide = pair.serverWritableSide;
+			clientReadableSide = pair.clientReadableSide;
 		});
+		// it('serverWritableSide instanceof OutgoingMessage', function(){
+		// 	assert(serverWritableSide instanceof http.OutgoingMessage);
+		// });
+		it('setHeader(name, value)', function(){
+			serverWritableSide.setHeader('Allow', 'GET, HEAD, POST');
+			serverWritableSide.end();
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.headers['allow'], 'GET, HEAD, POST');
+			});
+		});
+		it('getHeader(name)', function(){
+			serverWritableSide.setHeader('Allow', 'GET, HEAD, POST');
+			assert.strictEqual(serverWritableSide.getHeader('ALLOW'), 'GET, HEAD, POST');
+		});
+		it('getHeaderNames()', function(){
+			serverWritableSide.setHeader('Allow', 'GET, HEAD, POST');
+			serverWritableSide.setHeader('Content-Type', 'application/xhtml+xml');
+			assert.deepStrictEqual(serverWritableSide.getHeaderNames(), ['allow', 'content-type']);
+		});
+		it('getHeaders()', function(){
+			serverWritableSide.setHeader('Allow', 'GET, HEAD, POST');
+			serverWritableSide.setHeader('Content-Type', 'application/xhtml+xml');
+			const headers = serverWritableSide.getHeaders();
+			// FIXME is this expecting [Object: null prototype] ?
+			assert.deepStrictEqual(headers['allow'], 'GET, HEAD, POST');
+			assert.deepStrictEqual(headers['content-type'], 'application/xhtml+xml');
+		});
+		it('hasHeader()', function(){
+			serverWritableSide.setHeader('Allow', 'GET, HEAD, POST');
+			assert.strictEqual(serverWritableSide.hasHeader('ALLOW'), true);
+			assert.strictEqual(serverWritableSide.hasHeader('Foo'), false);
+		});
+		it('removeHeader()', function(){
+			serverWritableSide.setHeader('Allow', 'GET, HEAD, POST');
+			serverWritableSide.removeHeader('Allow');
+			assert.strictEqual(serverWritableSide.getHeader('Allow'), undefined);
+			assert.strictEqual(serverWritableSide.getHeader('ALLOW'), undefined);
+			assert.strictEqual(serverWritableSide.hasHeader('Allow'), false);
+		});
+		it('flushHeaders()', function(){
+			serverWritableSide.setHeader('Allow', 'GET, HEAD, POST');
+			serverWritableSide.flushHeaders();
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.headers['allow'], 'GET, HEAD, POST');
+			});
+		});
+		it('write()', function(done){
+			serverWritableSide.write('Content\r\n');
+			clientReadableSide.setEncoding('UTF-8');
+			clientReadableSide.on('readable', function(){
+				assert.strictEqual(this.read(), 'Content\r\n');
+				done();
+			});
+		});
+		it('end()', function(done){
+			serverWritableSide.end('Content\r\n');
+			clientReadableSide.setEncoding('UTF-8');
+			var data = '';
+			clientReadableSide.on('readable', function(){
+				for(var s; null !== (s=this.read());) data += s;
+			});
+			clientReadableSide.on('end', function(){
+				assert.strictEqual(data, 'Content\r\n');
+				done();
+			});
+		});
+		it('addTrailers()');
+		it('pipe()', function(done){
+			serverWritableSide.writeHead(400, 'Message', {Allow: 'GET, HEAD, POST'});
+			serverWritableSide.end('Content\r\n');
+
+			const pipe = lib.makeResponsePair();
+			clientReadableSide.pipe(pipe.serverWritableSide);
+
+			var data = '';
+			pipe.clientReadableSide.setEncoding('UTF-8');
+			pipe.clientReadableSide.on('headers', function(){
+				assert.strictEqual(pipe.clientReadableSide, this);
+				assert.strictEqual(this.statusCode, 400);
+				assert.strictEqual(this.statusMessage, 'Message');
+				assert.strictEqual(this.headers['allow'], 'GET, HEAD, POST');
+			});
+			pipe.clientReadableSide.on('readable', function(){
+				assert.strictEqual(pipe.clientReadableSide, this);
+				for(var s; null !== (s=this.read());) data += s;
+			});
+			pipe.clientReadableSide.on('end', function(){
+				assert.strictEqual(pipe.clientReadableSide, this);
+				assert.strictEqual(this.statusCode, 400);
+				assert.strictEqual(this.statusMessage, 'Message');
+				assert.strictEqual(this.headers['allow'], 'GET, HEAD, POST');
+				assert.strictEqual(data, 'Content\r\n');
+				done();
+			});
+		});
+	});
+	describe('IncomingMessage API', function(){
+		var pair, serverWritableSide, clientReadableSide;
+		beforeEach(function(){
+			pair = lib.makeResponsePair();
+			serverWritableSide = pair.serverWritableSide;
+			clientReadableSide = pair.clientReadableSide;
+			serverWritableSide.writeHead(400, 'Message', {Allow: 'GET, HEAD, POST'});
+			serverWritableSide.end('Content\r\n');
+		});
+		// it('clientReadableSide instanceof IncomingMessage', function(){
+		// 	assert(clientReadableSide instanceof http.IncomingMessage);
+		// });
+		it('rawTrailers');
+		it('trailers');
 		it('statusCode', function(){
-			iin.statusCode = 500;
-			assert.equal(iin.statusCode, 500);
-			iin.end();
-			assert.equal(iout.statusCode, undefined);
-		});
-		it('addHeader', function(){
-			iin.addHeader('Link', '<http://example.com/a>');
-			iin.addHeader('link', '<http://example.com/b>');
-			iin.addHeader('link', '<http://example.com/c>');
-			assert.equal(iin.getHeader('Link').length, 3);
-			iin.end();
-			assert(!iout.hasHeader('Link'));
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.statusCode, 400);
+			});
 		});
 		it('statusMessage', function(){
-			iin.statusMessage = 'Server Error';
-			assert.equal(iin.statusMessage, 'Server Error');
-			iin.end();
-			assert.equal(iout.statusMessage, '');
-		});
-		it('hasHeader', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			assert(iin.hasHeader('Content-Type'));
-			iin.end();
-			assert(!iout.hasHeader('Content-Type'));
-		});
-		it('getHeader', function(){
-			iin.setHeader('Content-Type', 'text/plain');
-			assert.equal(iin.getHeader('Content-Type'), 'text/plain');
-			iin.end();
-			assert.equal(iout.getHeader('Content-Type'), undefined);
-		});
-	});
-	describe('pipe (merging streams)', function(){
-		it('statusCode', function(){
-			var iin = new ResponsePassThrough;
-			var stream = new PassThrough;
-			var iout = new ResponsePassThrough;
-			iin.statusCode = 404;
-			iin.setHeader('Content-Type', 'text/plain');
-			stream.pipe(iin);
-
-			stream.write('File contents');
-			stream.end();
-
-			iin.pipe(iout);
-
-			return iout.ready.then(function(){
-				assert.equal(iout.statusCode, 404);
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.statusMessage, 'Message');
 			});
 		});
+		it('url', function(){
+			return clientReadableSide.ready.then(function(){
+				assert.strictEqual(clientReadableSide.url, '');
+			});
+		});
+		it('httpVersion');
+		it('headers', function(){
+			return clientReadableSide.ready.then(function(){
+				assert.deepStrictEqual(clientReadableSide.headers, {allow: 'GET, HEAD, POST'});
+			});
+		});
+		it('complete');
+		it('aborted');
+		it('setTimeout(msecs, callback)');
+		it('destroy(error)');
 	});
 });
