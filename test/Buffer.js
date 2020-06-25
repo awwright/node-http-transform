565dda3eb779e37d8e9508bca5139c96e02b6877
Austin Wright <aaa@bzfx.net>
2020-06-25 12:35:49 -0700

    Rename variables for consistency

--- test/Buffer.js
+++ test/Buffer.js
@@ -1,23 +1,23 @@
 "use strict";
 
 const inherits = require('util').inherits;
-const ServerResponseTransform = require('../lib/ResponseTransform.js').ResponseTransform;
+const ResponseTransform = require('../lib/ResponseTransform.js').ResponseTransform;
 
 // Pipe through an HTTP request - can serve to merge two streams together, e.g. to set Content-Type
 module.exports.ServerResponseBuffer = ServerResponseBuffer;
-inherits(ServerResponseBuffer, ServerResponseTransform);
+inherits(ServerResponseBuffer, ResponseTransform);
 function ServerResponseBuffer(){
 	if (!(this instanceof ServerResponseBuffer)){
 		return new ServerResponseBuffer();
 	}
-	ServerResponseTransform.apply(this, arguments);
+	ResponseTransform.apply(this, arguments);
 	this.headers = {};
 	this.body = '';
 }
 
 ServerResponseBuffer.prototype.setHeader = function(name, value) {
 	this.headers[name.toLowerCase()] = value;
-	ServerResponseTransform.prototype.setHeader.apply(this, arguments);
+	ResponseTransform.prototype.setHeader.apply(this, arguments);
 };
 
 ServerResponseBuffer.prototype._transformHead = function _transformHead(res, cb) {
